ba05b58553fa33643038d38af947b6a6f93eb200
Dhanuja S <samiveldhanuja@gmail.com>
2026-06-03 21:20:49 +0530

    Add files via upload

---
+++ Day 4/ex3.java
@@ -0,0 +1,12 @@
+public class ex3 {
+    public static void main(String[] args) {
+        int r=4,c=5;
+
+        for(int i=1;i<=r;i++) {
+            for(int j=1;j<=c;j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
